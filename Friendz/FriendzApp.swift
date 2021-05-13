b695dd8790d6d469813ee7b397514998e2bcd56c
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-05-13 08:47:00 +0200

    Initial Commit

---
+++ Friendz/FriendzApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendzApp.swift
+//  Friendz
+//
+//  Created by sanmi_personal on 13/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct FriendzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
